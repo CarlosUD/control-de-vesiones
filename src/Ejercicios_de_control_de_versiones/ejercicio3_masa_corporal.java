b22cab81efb26b582cf7f41334b9f3be2d23468d
carlosUD <edudelcid2001@gmail.com>
2021-05-03 14:40:57 -0600

    ejercio desarrollado el java

--- src/Ejercicios_de_control_de_versiones/ejercicio3_masa_corporal.java
+++ src/Ejercicios_de_control_de_versiones/ejercicio3_masa_corporal.java
@@ -1,92 +1,45 @@
 
 package Ejercicios_de_control_de_versiones;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-public class ejercicio3_masa_corporal {
+
+import java.util.Scanner;
+public class ejercicio3_masa_corporal{
+
+
+
+
+ 
     public static void main(String[] args) {
-      
-         try{
-              BufferedReader leer = new BufferedReader(new InputStreamReader(System.in));
-             System.out.println("ingrese la masa corporal");
-            double masa=Double.parseDouble(leer.readLine());
-            
-            ifmasacorporal(masa);
-           
-            
-       } catch(IOException | NumberFormatException e){
-           System.out.println(e.getMessage());
-       }    
-    }
-    public static void ifmasacorporal(double masa) throws IOException {
-         double peso;
-         double imc;
-         double imcq;
-        double altura;
-        if (masa <18) {
-            BufferedReader leer = new BufferedReader(new InputStreamReader(System.in));
-            System.out.println("ingrese el peso de la persona"); 
-            peso=Double.parseDouble(leer.readLine());
-            System.out.println("ingrese la alura de la persona");
-            altura=Double.parseDouble(leer.readLine());
-            imc= peso  / (altura)*2;
-            System.out.println("Peso bajo. Necesario valorar signos de desnutrición");
-            System.out.println("El IMC es: "+ imc);
-        } else if (masa >=18&& masa<=24.9) {
-            BufferedReader leer = new BufferedReader(new InputStreamReader(System.in));
-            System.out.println("ingrese el peso de la persona"); 
-            peso=Double.parseDouble(leer.readLine());
-            System.out.println("ingrese la alura de la persona");
-            altura=Double.parseDouble(leer.readLine());
-            imc= peso/altura*2;
-            System.out.println("normal");
-            System.out.println("El IMC es: "+ imc);
-        }else if (masa >=25&& masa<=26.9) {
-            BufferedReader leer = new BufferedReader(new InputStreamReader(System.in));
-            System.out.println("ingrese el peso de la persona"); 
-            peso=Double.parseDouble(leer.readLine());
-            System.out.println("ingrese la alura de la persona");
-            altura=Double.parseDouble(leer.readLine());
-            imc= peso/altura*2;
-            System.out.println("sobrepeso");
-            System.out.println("El IMC es: "+ imc);
-        }else if (masa >27) {
-            BufferedReader leer = new BufferedReader(new InputStreamReader(System.in));
-            System.out.println("ingrese el peso de la persona"); 
-            peso=Double.parseDouble(leer.readLine());
-            System.out.println("ingrese la alura de la persona");
-            altura=Double.parseDouble(leer.readLine());
-            imc= peso/altura*2;
-            System.out.println("Obesidad");
-            System.out.println("El IMC es: "+ imc);
-        }else if (masa>=27 && masa <29.9) {
-            BufferedReader leer = new BufferedReader(new InputStreamReader(System.in));
-            System.out.println("ingrese el peso de la persona"); 
-            peso=Double.parseDouble(leer.readLine());
-            System.out.println("ingrese la alura de la persona");
-            altura=Double.parseDouble(leer.readLine());
-            imc= peso/altura*2;
-            System.out.println("Obesidad grado I.Riesgo relativoaltopara desarrollar enfermedades cardiovasculares");
-            System.out.println("El IMC es: "+ imc);
-        }else if (masa >=30&&masa <=39.9) {
-            BufferedReader leer = new BufferedReader(new InputStreamReader(System.in));
-            System.out.println("ingrese el peso de la persona"); 
-            peso=Double.parseDouble(leer.readLine());
-            System.out.println("ingrese la alura de la persona");
-            altura=Double.parseDouble(leer.readLine());
-            imc= peso/altura*2;
-            System.out.println("Obesidad grado II.Riesgo relativomuy altopara el desarrollode enfermedades cardiovasculares");
-            System.out.println("El IMC es: "+ imc);
-        }else if (masa>=40) {
-            BufferedReader leer = new BufferedReader(new InputStreamReader(System.in));
-            System.out.println("ingrese el peso de la persona"); 
-            peso=Double.parseDouble(leer.readLine());
-            System.out.println("ingrese la alura de la persona");
-            altura=Double.parseDouble(leer.readLine());
-            imc= peso/altura*2;
-            System.out.println("Obesidad grado III Extrema o Mórbida.Riesgo relativoextremadamente altopara el desarrollo de enfermedades cardiovasculares");
-            System.out.println("El IMC es: "+ imc);
+       double peso,estatura,imc;
+       String masa = "";
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Escribe tu peso en kilogramos");
+        peso = Double.parseDouble(leer.nextLine());
+        System.out.println("Escribe tu estatura en metros");
+        estatura= Double.parseDouble(leer.nextLine());
+        imc=peso /(estatura*estatura);
+        
+        if (imc<=18) {
+            masa = "Peso bajo. Necesario valorar signos \n de desnutrición";
         }
+        if(imc>=18 && imc<=24.9) {
+            masa= "Normal";
+        }
+        if (imc>=25 && imc<=26.9) {
+            masa= "Sobrepeso";
+        }
+        if (imc>=27) {
+            masa= "Obesidad";
+        }
+        if (imc>=27.9 && imc<=29.9) {
+            masa ="Obesidad grado I.Riesgo relativoaltopara desarrollar enfermedades cardiovasculares";
+        }
+        if (imc>=30 && imc<=39.9) {
+            masa ="Obesidad grado II.Riesgo relativomuy altopara el desarrollode enfermedades cardiovasculares";
+        }
+        if (imc>=40) {
+            masa ="Obesidad grado III Extrema o Mórbida.Riesgo relativoextremadamente altopara el desarrollo de enfermedades cardiovasculares";
+        }
+        System.out.println("Tu Indice de masa corporal es: "+imc+" \n\n TIENE:"+ masa);
     }
 }
